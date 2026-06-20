5d7a7a370bb719b17e159b4d28288728e73758a8
Irvine <irvinemay7@gmail.com>
2026-06-20 12:14:42 +0800

    Updated my images and layout

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3578136295" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "203180316" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
